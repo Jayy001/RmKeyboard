9566c421fb4591ebcbef788e7e181338b876ad0a
Jacob B <56607897+Jayy001@users.noreply.github.com>
2023-04-13 18:02:00 +0100

    v1 Release (Added support for SSL, logging & user arguments)

--- client.py
+++ client.py
@@ -1,47 +1,117 @@
-import sys
 import struct
-import socket
 import keyboard
-import time
-
-import asyncio
 import logging
-from websockets.sync.client import connect
+import ssl
+import websocket
+import sys
+import argparse
+import rel
+import sys
 
-logging.basicConfig(level=logging.DEBUG)
+
+def hook(event):
+    logging.debug(f"Caught event: {event.event_type}, {event.scan_code}, {event.name}")
+    if event.event_type == keyboard.KEY_DOWN:
+        emit(EV_KEY, event.scan_code, 1)
+    elif event.event_type == keyboard.KEY_UP:
+        emit(EV_KEY, event.scan_code, 0)
+    else:
+        return
+    emit(EV_SYN, SYN_REPORT, 0)
 
 
-def main(port=8765):
-    address = sys.argv[-1] if len(sys.argv) > 1 else "10.11.99.1"
+def emit(type_, code, value):
+    logging.debug(f"Sent event: {type_}, {code}, {value}")
+    wsapp.send(struct.pack("IIi", type_, code, value))
+
+
+def on_open(wsapp):
+    logging.info(f"Authenticating with: {token}")
+    wsapp.send(token)
+
+
+def on_message(wsapp, message):
+    if message == "OK":
+        logging.info("Authentication successful")
+        keyboard.hook(hook)
+    else:
+        logging.debug(f"Message recieved: {message}")
+
+
+def on_close(wsapp, close_status_code, close_msg):
+    logging.error(f"WebSocket closed: [{close_status_code}] {close_msg}")
+    rel.abort()
+    exit(1)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="RmKeyboard Client",
+        description="Basic WebSocket client for the RmKeyboard server",
+        epilog="Example: python3 client.py 10.11.99.1 password",
+    )
+    parser.add_argument(
+        "-a",
+        "--address",
+        help="The address of the websocket server [10.11.99.1]",
+        default="10.11.99.1",
+    )
+    parser.add_argument(
+        "-t",
+        "--token",
+        help="The token for the websocket server [Password]",
+        default="Password",
+    )
+    parser.add_argument(
+        "-p", "--port", help="The port for the websocket server [8765]", default=8765
+    )
+    parser.add_argument(
+        "-s",
+        "--secure",
+        help="Use secure socket layer",
+        action="store_true",
+        default=False,
+    )
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="Enable verbose logging"
+    )
+
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    address = args.address
+    port = args.port
+    token = args.token
+
+    sslopt = {}
+    if args.secure:
+        logging.info("Using SSL")
+
+        ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+        ssl_context.load_verify_locations("certs/cert.pem")
+        sslopt["context"] = ssl_context
+
+        protocol = "wss"
+    else:
+        protocol = "ws"
 
     EV_KEY = 0x01
     EV_SYN = 0x00
     SYN_REPORT = 0x00
 
+    wsapp = websocket.WebSocketApp(
+        f"{protocol}://{address}:{port}",
+        on_open=on_open,
+        on_message=on_message,
+        on_close=on_close,
+    )
+
     logging.info(f"Attempting to connect to {address}:{port}")
-    with connect(f"ws://{address}:{port}") as websocket:
-        logging.info(f"Connection sucessfull")
+    wsapp.run_forever(sslopt=sslopt, dispatcher=rel)
 
-        def emit(type_, code, value):
-            logging.debug(f"Sent event: {type_}, {code}, {value}")
-            websocket.send(struct.pack("IIi", type_, code, value))
-
-        def hook(event):
-            logging.debug(
-                "Caught event: {event.event_type}, {event.scan_code}, {event.name}"
-            )
-            if event.event_type == keyboard.KEY_DOWN:
-                emit(EV_KEY, event.scan_code, 1)
-            elif event.event_type == keyboard.KEY_UP:
-                emit(EV_KEY, event.scan_code, 0)
-            else:
-                return
-
-            emit(EV_SYN, SYN_REPORT, 0)
-
-        keyboard.hook(hook)
-        keyboard.wait()
-
-
-if __name__ == "__main__":
-    main()
+    rel.signal(2, rel.abort)  # Keyboard Interrupt
+    rel.dispatch()
--- server.py
+++ server.py
@@ -2,32 +2,93 @@ import struct
 import uinput
 import logging
 import asyncio
+import signal
+import ssl
+import websockets
+import sys
+import argparse
 
-from websockets.server import serve
 
-logging.basicConfig(level=logging.DEBUG)
+async def inject(websocket):
+    token = await websocket.recv()
+
+    if token != pwd:
+        await websocket.close(1011, "Unauthorized (Wrong token)")
+    else:
+        await websocket.send("OK")
+
+    try:
+        async for event in websocket:
+            type_, code, value = struct.unpack("IIi", bytes(event.encode("utf-8")))
+
+            logging.debug(f"Emitting event: {(type_, code), value}")
+            device.emit((type_, code), value, False)
+    except websockets.exceptions.ConnectionClosed:
+        logging.error("Client disconnected")
+
+
+async def main():
+    logging.debug("Attempting to start WebSocket")
+
+    loop = asyncio.get_running_loop()
+    stop = loop.create_future()
+    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+
+    if ssl_check:
+        logging.info("Using SSL")
+        ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+        ssl_context.load_cert_chain("certs/cert.pem", keyfile="certs/key.pem")
+    else:
+        ssl_context = None
+
+    async with websockets.serve(
+        inject, address, port, ssl=ssl_context, compression=None
+    ):
+        logging.info(f"WebSocket running: {address}, {port}")
+
+        await stop
+
+
+parser = argparse.ArgumentParser(
+    prog="RmKeyboard Server",
+    description="RmKeyboard Server to recieve events from the client over a websocket and inject them into the virtual device",
+    epilog="Example: python3 server.py Password",
+)
+parser.add_argument(
+    "-a", "--address", help="The address to bind to [0.0.0.0]", default="0.0.0.0"
+)
+parser.add_argument(
+    "-t",
+    "--token",
+    help="The token for the websocket server [Password]",
+    default="Password",
+)
+parser.add_argument(
+    "-p", "--port", help="The port for the websocket server [8765]", default=8765
+)
+parser.add_argument(
+    "-s", "--secure", help="Use secure socket layer", action="store_true", default=False
+)
+parser.add_argument(
+    "-v", "--verbose", action="store_true", help="Enable verbose logging"
+)
+
+args = parser.parse_args()
+
+if args.verbose:
+    logging.basicConfig(level=logging.DEBUG)
+else:
+    logging.basicConfig(level=logging.INFO)
+
+pwd = args.token
+address = args.address
+port = args.port
+ssl_check = args.secure
 
 device = uinput.Device(
     [getattr(uinput, x) for x in dir(uinput) if x.startswith("KEY_")]
 )
 logging.debug(f"Using device events: {device._Device__events}")
-
-
-async def inject(websocket):
-    async for event in websocket:
-        type_, code, value = struct.unpack("IIi", event)
-
-        logging.debug(f"Emitting event: {(type_, code), value}")
-        device.emit((type_, code), value, False)
-
-
-async def main(address="0.0.0.0", port=8765):
-    logging.debug("Attempting to start WebSocket")
-
-    async with serve(inject, address, port):
-        logging.info(f"WebSocket running: {address}, {port}")
-
-        await asyncio.Future()
-
+logging.debug(f"Using token: {pwd}")
 
 asyncio.run(main())
